c121a430eb296b04227e1b7b860cac1aa5f68cf1
shiavaregalla99 <shivaregalla99@gmail.com>
2022-08-20 20:02:41 +0530

    this is my fourth commit

--- src/test/java/com/magento/pages/LoginPageElements.java
+++ src/test/java/com/magento/pages/LoginPageElements.java
@@ -1,6 +1,6 @@
 package com.magento.pages;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
